aa95bfc1d62c03fd574197f60e045c5c9ae7f49f
hangnhancs <thaihoangnhantk17lqd@gmail.com>
2025-04-25 23:34:02 +0700

    update

--- client/src/components/AddNewAddressDialog.tsx
+++ client/src/components/AddNewAddressDialog.tsx
@@ -152,7 +152,7 @@ export default function AddNewAddressDialog({ open, onClose}: Props ) {
                     Hủy
                 </Button>
                 <Button onClick={handleAddNewAddress} variant="contained" color="primary">
-                    Lưu địa chỉ
+                    Lưu địa chỉi
                 </Button>
             </DialogActions>
         </Dialog>
